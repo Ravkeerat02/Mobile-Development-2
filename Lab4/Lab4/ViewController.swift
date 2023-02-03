5aeb219eee0c26d9190ec41d6303597a6e73a9b7
Ravkeerat02 <42888466+Ravkeerat02@users.noreply.github.com>
2023-02-03 02:47:19 -0500

    Storyboard implemented

---
+++ Lab4/Lab4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab4
+//
+//  Created by laptop on 2023-02-03.
+//  Copyright © 2023 laptop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
